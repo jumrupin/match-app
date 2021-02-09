67926409ef6b971765599a9f1c24a4c2c71a9c21
Jumrupin <junpei12131018@gmail.com>
2021-02-09 22:47:50 +0900

    Add enum

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, key: [:name, :gender])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender])
   end
 
 end
