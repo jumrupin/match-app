3f2fa78d52bbcf7d88078a1e1b6a3ef26db44b21
Jumrupin <junpei12131018@gmail.com>
2021-02-15 13:50:02 +0900

    Fix redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, 
+  controllers: { 
+    registrations: 'registrations',
+    sessions: :sessions
+  }
 
   root 'top#index'
 
   resources :users, only: [:show, :index]
+  
 end
